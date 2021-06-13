eefbc8038dc36b7683448994988b7a88d40705c3
Jongmin Yun <jmyundev@gmail.com>
2021-06-13 22:20:08 +0900

    Expand `Extent2` `IExtent2` `Extent3` and `IExtent3` functionalities.

--- src/extent.rs
+++ src/extent.rs
@@ -68,8 +68,57 @@ impl Extent2 {
         } else if height < 0f32 {
             Err(EError::NegativeLength(height))
         } else {
-            Ok(Self { width, height })
+            Ok(Self::uncheck_new(width, height))
         }
+    }
+
+    /// Create new [Extent2] but not check whether given `length` is negative or not.
+    /// If value is negative, the application will be halted instantly.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use hamilton as math;
+    /// use math::Extent2;
+    ///
+    /// let extent = Extent2::uncheck_from_scalar(32f32);
+    /// assert_eq!(extent.width(), 32f32);
+    /// assert_eq!(extent.height(), 32f32);
+    /// ```
+    ///
+    /// ``` should_panic
+    /// use hamilton as math;
+    /// use math::Extent2;
+    ///
+    /// let _panicked = Extent2::uncheck_from_scalar(-1f32);
+    /// ```
+    pub fn uncheck_from_scalar(length: f32) -> Self {
+        Self::uncheck_new(length, length)
+    }
+
+    /// Create new [Extent2] but check given `length` is positive or 0.
+    /// If any value is negative, return error value instead.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use hamilton as math;
+    /// use math::Extent2;
+    ///
+    /// let extent = Extent2::from_scalar(32f32).unwrap();
+    /// assert_eq!(extent.width(), 32f32);
+    /// assert_eq!(extent.height(), 32f32);
+    /// ```
+    ///
+    /// ```
+    /// use hamilton as math;
+    /// use math::Extent2;
+    ///
+    /// let should_err = Extent2::from_scalar(-1f32).is_err();
+    /// assert_eq!(should_err, true);
+    /// ```
+    pub fn from_scalar(length: f32) -> Result<Self, EError> {
+        Self::new(length, length)
     }
 
     /// Return `width` value of the extent.
@@ -80,6 +129,11 @@ impl Extent2 {
     /// Return `height` value of the extent.
     pub fn height(&self) -> f32 {
         self.height
+    }
+
+    /// Return area value of this extent.
+    pub fn area(&self) -> f32 {
+        self.width * self.height
     }
 
     /// Convert into [IExtent2] but any fractional values are truncated.
@@ -139,6 +193,22 @@ impl IExtent2 {
         Self { width, height }
     }
 
+    /// Create new [IExtent2]
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use hamilton as math;
+    /// use math::IExtent2;
+    ///
+    /// let extent = IExtent2::from_scalar(32);
+    /// assert_eq!(extent.width(), 32);
+    /// assert_eq!(extent.height(), 32);
+    /// ```
+    pub fn from_scalar(length: u32) -> Self {
+        Self::new(length, length)
+    }
+
     /// Return `width` value of the extent.
     pub fn width(&self) -> u32 {
         self.width
@@ -147,6 +217,11 @@ impl IExtent2 {
     /// Return `height` value of the extent.
     pub fn height(&self) -> u32 {
         self.height
+    }
+
+    /// Get area of extent.
+    pub fn area(&self) -> u64 {
+        (self.width as u64) * (self.height as u64)
     }
 }
 
@@ -234,12 +309,59 @@ impl Extent3 {
         } else if depth < 0f32 {
             Err(EError::NegativeLength(depth))
         } else {
-            Ok(Self {
-                width,
-                height,
-                depth,
-            })
+            Ok(Self::uncheck_new(width, height, depth))
         }
+    }
+
+    /// Create new [Extent3] but not check whether given `length` is negative or not.
+    /// If value is negative, the application will be halted instantly.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use hamilton as math;
+    /// use math::Extent3;
+    ///
+    /// let extent = Extent3::uncheck_from_scalar(32f32);
+    /// assert_eq!(extent.width(), 32f32);
+    /// assert_eq!(extent.height(), 32f32);
+    /// assert_eq!(extent.depth(), 32f32);
+    /// ```
+    ///
+    /// ``` should_panic
+    /// use hamilton as math;
+    /// use math::Extent3;
+    ///
+    /// let _panicked = Extent3::uncheck_from_scalar(-1f32);
+    /// ```
+    pub fn uncheck_from_scalar(length: f32) -> Self {
+        Self::uncheck_new(length, length, length)
+    }
+
+    /// Create new [Extent3] but check given `length` is positive or 0.
+    /// If any value is negative, return error value instead.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use hamilton as math;
+    /// use math::Extent3;
+    ///
+    /// let extent = Extent3::from_scalar(32f32).unwrap();
+    /// assert_eq!(extent.width(), 32f32);
+    /// assert_eq!(extent.height(), 32f32);
+    /// assert_eq!(extent.depth(), 32f32);
+    /// ```
+    ///
+    /// ```
+    /// use hamilton as math;
+    /// use math::Extent3;
+    ///
+    /// let should_err = Extent3::from_scalar(-1f32).is_err();
+    /// assert_eq!(should_err, true);
+    /// ```
+    pub fn from_scalar(length: f32) -> Result<Self, EError> {
+        Self::new(length, length, length)
     }
 
     /// Return `width` value of the extent.
@@ -255,6 +377,16 @@ impl Extent3 {
     /// Return `depth` value of the extent.
     pub fn depth(&self) -> f32 {
         self.depth
+    }
+
+    /// Get surface area of the extent.
+    pub fn surface_area(&self) -> f32 {
+        2f32 * ((self.width * self.height) + (self.height * self.depth) + (self.depth * self.width))
+    }
+
+    /// Get volume of the extent.
+    pub fn volume(&self) -> f32 {
+        self.width * self.height * self.depth
     }
 
     /// Convert into [IExtent3] but any fractional values are truncated.
@@ -328,6 +460,23 @@ impl IExtent3 {
         }
     }
 
+    /// Create new [IExtent3]
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use hamilton as math;
+    /// use math::IExtent3;
+    ///
+    /// let extent = IExtent3::from_scalar(32);
+    /// assert_eq!(extent.width(), 32);
+    /// assert_eq!(extent.height(), 32);
+    /// assert_eq!(extent.depth(), 32);
+    /// ```
+    pub fn from_scalar(length: u32) -> Self {
+        Self::new(length, length, length)
+    }
+
     /// Return `width` value of the extent.
     pub fn width(&self) -> u32 {
         self.width
@@ -341,6 +490,20 @@ impl IExtent3 {
     /// Return `depth` value of the extent.
     pub fn depth(&self) -> u32 {
         self.depth
+    }
+
+    /// Get surface area of the extent.
+    pub fn surface_area(&self) -> u64 {
+        let width = self.width as u64;
+        let height = self.height as u64;
+        let depth = self.depth as u64;
+
+        2u64 * ((width * height) + (height * depth) + (depth * width))
+    }
+
+    /// Get volume of extent.
+    pub fn volume(&self) -> u64 {
+        (self.width as u64) * (self.height as u64) * (self.depth as u64)
     }
 }
 
